a6d768c387e6f60af30b942ac70d50a016984b19
Andrei Scafi <andreiscafi@hotmail.com>
2023-06-29 09:07:17 -0300

    Adding iPhone cube HTML structure.

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+/* My own reproduction of the code without following the tutorial */
+
+/* const slideshowDiv = () => {
+    for (let i = 1; i <= 5; i++) {
+
+        const div = document.createElement('div');
+
+        div.style.backgroundImage = `url('images/slideshow/section-1-bg-${i}.jpg')`;
+
+        i === 1 && div.classList.add('change');
+
+        document.querySelector('.slideshow').appendChild(div);
+
+    }
+}
+
+slideshowDiv();
+
+const slideshowDivs = document.querySelectorAll('.slideshow div');
+
+let a = 1;
+
+const slideshow = () => {
+    setInterval(() => {
+        a++;
+
+        const div = document.querySelector('.slideshow .change');
+
+        div.classList.remove('change');
+
+        if (a <= slideshowDivs.length) {
+            div.nextElementSibling.classList.add('change');
+        } else {
+            slideshowDivs[0].classList.add('change');
+            a = 1;
+        }
+
+    }, 20000)
+}
+
+slideshow(); */
+
+/* End My own reproduction of the code without following the tutorial */
+
 /* Slideshow */
 
 const slideshowDivs = () => {
@@ -44,46 +88,3 @@ slideshow();
 
 /* End of Slideshow */
 
-/* My own reproduction of the code without following the tutorial */
-
-/* const slideshowDiv = () => {
-    for (let i = 1; i <= 5; i++) {
-
-        const div = document.createElement('div');
-
-        div.style.backgroundImage = `url('images/slideshow/section-1-bg-${i}.jpg')`;
-
-        i === 1 && div.classList.add('change');
-
-        document.querySelector('.slideshow').appendChild(div);
-
-    }
-}
-
-slideshowDiv();
-
-const slideshowDivs = document.querySelectorAll('.slideshow div');
-
-let a = 1;
-
-const slideshow = () => {
-    setInterval(() => {
-        a++;
-
-        const div = document.querySelector('.slideshow .change');
-
-        div.classList.remove('change');
-
-        if (a <= slideshowDivs.length) {
-            div.nextElementSibling.classList.add('change');
-        } else {
-            slideshowDivs[0].classList.add('change');
-            a = 1;
-        }
-
-    }, 20000)
-}
-
-slideshow(); */
-
-/* End My own reproduction of the code without following the tutorial */
