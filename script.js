4e47025e85d3399d76b75bd823e1ab0b279ede5a
Andrei Scafi <andreiscafi@hotmail.com>
2023-06-28 23:17:16 -0300

    Adding slideshow div elements using javascript.

---
+++ script.js
@@ -0,0 +1,16 @@
+/* Slideshow */
+
+const slideshowDivs = () => {
+    for (let i = 1; i <= 5; i++) {
+
+        const div = document.createElement('div');
+
+        div.style.backgroundImage = `url(images/slideshow/section-1-bg-${i}.jpg)`;
+
+        document.querySelector('.slideshow').appendChild(div);
+    }
+}
+
+slideshowDivs();
+
+/* End of Slideshow */
