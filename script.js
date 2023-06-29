0e44daad01cabdc12db80e6a7a669e2d1bc5fadd
Andrei Scafi <andreiscafi@hotmail.com>
2023-06-28 23:49:27 -0300

    Adding transition effect to slideshow with javascript.

--- script.js
+++ script.js
@@ -19,4 +19,27 @@ const slideshowDivs = () => {
 
 slideshowDivs();
 
+const divs = document.querySelectorAll('.slideshow div');
+
+let a = 1;
+
+const slideshow = () => {
+    setInterval(() => {
+        a++;
+
+        const div = document.querySelector('.slideshow .change');
+
+        div.classList.remove('change');
+
+        if (a <= divs.length) {
+            div.nextElementSibling.classList.add('change')
+        } else {
+            divs[0].classList.add('change')
+            a = 1;
+        }
+    }, 20000)
+}
+
+slideshow();
+
 /* End of Slideshow */
