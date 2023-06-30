553346fa58e653de87362e8bc047c474dd5d6965
Andrei Scafi <andreiscafi@hotmail.com>
2023-06-30 10:40:10 -0300

    Adding functionality to cube controls with javascript.

--- script.js
+++ script.js
@@ -47,10 +47,38 @@ slideshow(); */
 const cube = document.querySelector('.cube');
 
 let y = 0;
+let x = 0;
+let z = 0;
+
+document.querySelector('.top-x-control').addEventListener('click', () => {
+    cube.style.transform = `rotateX(${x += 20}deg) rotateY(${y}deg) rotateZ(${z}deg)`
+});
+
+document.querySelector('.bottom-x-control').addEventListener('click', () => {
+    cube.style.transform = `rotateX(${x -= 20}deg) rotateY(${y}deg) rotateZ(${z}deg)`
+})
+
+document.querySelector('.left-y-control').addEventListener('click', () => {
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y -= 20}deg) rotateZ(${z}deg)`
+});
+
+document.querySelector('.right-y-control').addEventListener('click', () => {
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y += 20}deg) rotateZ(${z}deg)`
+});
+
+document.querySelector('.top-z-control').addEventListener('click', () => {
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${z -= 20}deg)`
+});
+
+document.querySelector('.bottom-z-control').addEventListener('click', () => {
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${z += 20}deg)`
+});
+
+
 
 const playPause = () => {
     setInterval(() => {
-        cube.style.transform = `rotateY(${y++}deg)`
+        cube.style.transform = `rotateX(${x}deg) rotateY(${y++}deg) rotateZ(${z}deg)`
     }, 100);
 }
 
